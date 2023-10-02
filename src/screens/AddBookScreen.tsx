675a282a6f4579a7c95111e8daf113f8623db117
Muzaib <muhammadmuzaib43@gmail.com>
2023-10-01 22:20:31 -0400

    BS-33 Feature: adding more screens. Time spent: 7hours

--- src/screens/AddBookScreen.tsx
+++ src/screens/AddBookScreen.tsx
@@ -1,11 +1,11 @@
 import React, {useState} from 'react';
-import {View, Button, Image} from 'react-native';
+import {View, Image} from 'react-native';
+import {Button, useTheme, Box} from 'native-base';
 import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
 import {
   Input,
   Select,
   VStack,
-  CheckIcon,
   FormControl,
   WarningOutlineIcon,
   Center,
@@ -13,11 +13,15 @@ import {
 import styles from '../styles/AddBookScreenStyles';
 
 export default function AddBookScreen() {
+  const theme = useTheme();
   const [imgUrl, setImgUrl] = useState<string | null>(null);
   const [isbn, setIsbn] = useState<string>('');
   const [author, setAuthor] = useState<string>('');
   const [bookName, setBookName] = useState<string>('');
-  const [service, setService] = React.useState('');
+  const [GenreType, setGenreType] = useState<string | null>('');
+  const [ServiceType, setServiceType] = useState<string | null>('');
+  const [isGenreInvalid, setGenreIsInvalid] = useState(true);
+  const [isServiceInvalid, setServiceIsInvalid] = useState(true);
 
   const openCamera = async () => {
     const result = await launchCamera({
@@ -40,8 +44,28 @@ export default function AddBookScreen() {
     console.log('Result=>2', result);
   };
 
+  const handleGenreSelect = (value: string | null) => {
+    setGenreType(value);
+    setGenreIsInvalid(!value);
+  };
+
+  const handleServiceSelect = (value: string | null) => {
+    setServiceType(value);
+    setServiceIsInvalid(!value);
+  };
+
+  const handleSubmit = () => {
+    console.log('Form submitted');
+    console.log('Image URL:', imgUrl);
+    console.log('ISBN:', isbn);
+    console.log('Author:', author);
+    console.log('Book Name:', bookName);
+    console.log('Genre Type:', GenreType);
+    console.log('Genre Type:', ServiceType);
+  };
+
   return (
-    <View style={styles.container}>
+    <Box backgroundColor={theme.colors.primary[300]} style={styles.container}>
       <View>
         <Image
           resizeMode="contain"
@@ -49,8 +73,12 @@ export default function AddBookScreen() {
           source={imgUrl ? {uri: imgUrl} : require('../images/book3.jpg')}
         />
       </View>
-      <Button title="Pick image" onPress={ImagePicker} />
-      <Button title="Open Camera" onPress={openCamera} />
+      <Button style={styles.buttons} onPress={ImagePicker}>
+        Pick image
+      </Button>
+      <Button style={styles.buttons} onPress={openCamera}>
+        Open Camera
+      </Button>
       <VStack space={2} width="100%">
         <Input
           placeholder="ISBN"
@@ -68,22 +96,24 @@ export default function AddBookScreen() {
           onChangeText={text => setBookName(text)}
         />
         <Center>
-          <FormControl w="3/4" maxW="300" isRequired isInvalid isReadOnly>
-            <FormControl.Label>Choose service</FormControl.Label>
+          <FormControl
+            w="100%"
+            isRequired
+            isInvalid={isServiceInvalid}
+            isReadOnly>
+            <FormControl.Label>Choose service: Rent or Buy</FormControl.Label>
             <Select
-              minWidth="200"
+              width="100%"
               accessibilityLabel="Choose Service"
               placeholder="Choose Service"
+              color={theme.colors.primary[400]}
               _selectedItem={{
                 bg: 'teal.600',
-                endIcon: <CheckIcon size={5} />,
               }}
-              mt="1">
-              <Select.Item label="UX Research" value="ux" />
-              <Select.Item label="Web Development" value="web" />
-              <Select.Item label="Cross Platform Development" value="cross" />
-              <Select.Item label="UI Designing" value="ui" />
-              <Select.Item label="Backend Development" value="backend" />
+              mt="1"
+              onValueChange={handleServiceSelect}>
+              <Select.Item label="Rent" value="rent" />
+              <Select.Item label="Gift" value="gift" />
             </Select>
             <FormControl.ErrorMessage
               leftIcon={<WarningOutlineIcon size="xs" />}>
@@ -91,7 +121,46 @@ export default function AddBookScreen() {
             </FormControl.ErrorMessage>
           </FormControl>
         </Center>
+
+        <Center>
+          <FormControl
+            w="100%"
+            isRequired
+            isInvalid={isGenreInvalid}
+            isReadOnly>
+            <FormControl.Label>Choose Genre</FormControl.Label>
+            <Select
+              width="100%"
+              accessibilityLabel="Choose Genre"
+              placeholder="Choose Genre"
+              color={theme.colors.primary[400]}
+              _selectedItem={{
+                bg: 'teal.600',
+              }}
+              mt="1"
+              onValueChange={handleGenreSelect}>
+              <Select.Item label="Literary Fiction" value="literary_fiction" />
+              <Select.Item
+                label="Historical Fiction"
+                value="historical_fiction"
+              />
+              <Select.Item
+                label="Mystery/Thriller/Crime"
+                value="mystery_thriller_crime"
+              />
+              <Select.Item label="Science Fiction" value="science_fiction" />
+              <Select.Item label="Fantasy" value="fantasy" />
+            </Select>
+            <FormControl.ErrorMessage
+              leftIcon={<WarningOutlineIcon size="xs" />}>
+              Please make a selection!
+            </FormControl.ErrorMessage>
+          </FormControl>
+        </Center>
+        <Box style={styles.submitButtonWrapper}>
+          <Button onPress={handleSubmit}>Submit</Button>
+        </Box>
       </VStack>
-    </View>
+    </Box>
   );
 }
